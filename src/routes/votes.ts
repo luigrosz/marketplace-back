6ea67ed276ffb950aa2f807da1640447931c2911
Lui Grosz <luigrosz08@gmail.com>
2025-05-22 13:00:36 -0300

    votes

--- src/routes/votes.ts
+++ src/routes/votes.ts
@@ -1,10 +1,11 @@
 import { Router, Request, Response } from 'express';
+import pool from '../db/pool';
 
 const router = Router();
 
 interface Vote {
-	user_id: number;
-	product_id: number;
-	vote_type: number;
-	created_at: number;
+  user_id: number;
+  product_id: number;
+  vote_type: number; // 1 for upvote, -1 for downvote
+  created_at: number;
 }
